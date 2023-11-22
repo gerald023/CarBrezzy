61b311de56f4c13090e6fe56e7438a7ce9c98114
gerald <lekaragerald19@gmail.com>
2023-11-23 00:59:16 +0100

    adding styles

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -3,18 +3,35 @@ import { Router } from 'express';
 import { CommonModule } from '@angular/common';
 import {  } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars, faHome, faUser,faWarehouse, faHeadset,faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
   imports: [CommonModule, FontAwesomeModule],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrls: [
+    './nav.component.css',
+    './menu.css'
+  ]
 })
 export class NavComponent {
- 
-  toggleItems(){
-    console.log('clicked me');
-    
+
+  faBar = faBars;
+  fahome = faHome;
+  fauser = faUser;
+  fashop = faWarehouse;
+  facontact = faHeadset;
+  faclose = faXmark;
+
+  isMenuOpen:boolean = false;
+  toggleMenu(){
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+  clickedOutside(){
+    this.isMenuOpen = false;
+  }
+  clickedInside(){
+    this.isMenuOpen = true;
   }
 }
