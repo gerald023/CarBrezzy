054ae10d9c4b16a582eea849ca5563838e9a9a43
gerald <lekaragerald19@gmail.com>
2023-11-23 17:26:12 +0100

    adding styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { ContactComponent } from './contact/contact.component';
 import { ProductslistComponent } from './products/productslist/productslist.component';
 import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'contact', component: ContactComponent},
     {path: 'shop', component: ProductslistComponent},
     {path: 'about', component: AboutUsComponent},
-    {path: 'shop/:id', component: ProductDetailComponent}
+    {path: 'shop/:id', component: ProductDetailComponent},
+    {path: '**', component: ErrorPageComponent},
 ];
--- src/app/contact/faq/faq.component.ts
+++ src/app/contact/faq/faq.component.ts
@@ -1,21 +1,27 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-faq',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.css'
 })
 export class FaqComponent {
   @Input() data:any;
 
+
   isFaqOpen: boolean = false;
   toggleFaq(){
     this.isFaqOpen = !this.isFaqOpen;
   }
+  
   constructor(){}
 
+  angleUp = faAngleUp;
+  angleDown = faAngleDown;
 
 }
